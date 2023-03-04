a6e4662a6120c7c5531f4ceaa3821decba1cab21
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2023-03-04 20:36:13 -0300

    Inserindo novos Testes

--- src/test/java/one/digitalinnovationon/junit/PessoaTest.java
+++ src/test/java/one/digitalinnovationon/junit/PessoaTest.java
@@ -12,4 +12,16 @@ public class PessoaTest {
         Pessoa pessoa = new Pessoa("Julia", LocalDate.of(2020,1,1));
         Assertions.assertEquals(3,pessoa.getIdade());
     }
+
+    @Test
+    void deveRetornarSeEhMaiorDeIdade(){
+        Pessoa julia = new Pessoa("Julia", LocalDate.of(2000,1,1));
+        Assertions.assertTrue(julia.ehMaiorDeIdade());
+
+        Pessoa joao = new Pessoa("Joao", LocalDate.now());
+        Assertions.assertFalse(joao.ehMaiorDeIdade());
+
+
+
+    }
 }
