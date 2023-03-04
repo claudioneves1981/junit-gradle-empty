4ff765a61d80ed4baafe6319c71edc2d0a5e394e
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2023-03-04 20:59:52 -0300

    Ajustando os imports do projeto, e inserindo mais testes.

--- src/test/java/one/digitalinnovationon/junit/PessoaTest.java
+++ src/test/java/one/digitalinnovationon/junit/PessoaTest.java
@@ -1,24 +1,35 @@
 package one.digitalinnovationon.junit;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PessoaTest {
 
     @Test
     void validarCalculoDeIdade(){
         Pessoa pessoa = new Pessoa("Julia", LocalDate.of(2020,1,1));
-        Assertions.assertEquals(3,pessoa.getIdade());
+        assertEquals(3,pessoa.getIdade());
     }
 
     @Test
     void deveRetornarSeEhMaiorDeIdade(){
         Pessoa julia = new Pessoa("Julia", LocalDate.of(2000,1,1));
-        Assertions.assertTrue(julia.ehMaiorDeIdade());
+        assertTrue(julia.ehMaiorDeIdade());
 
         Pessoa joao = new Pessoa("Joao", LocalDate.now());
-        Assertions.assertFalse(joao.ehMaiorDeIdade());
+        assertFalse(joao.ehMaiorDeIdade());
+    }
+
+    @Test
+    void validarSeObjetoEstaNulo(){
+        Pessoa pessoa = null;
+        assertNull(pessoa);
+
+        pessoa = new Pessoa("Luciano", LocalDate.now());
+        assertNotNull(pessoa);
     }
 }
